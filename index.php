87e20a7c717aa1a32aeed628556962135c21374e
Rainier Ababao <rainier.ababao@gmail.com>
2015-03-30 13:41:40 -0700

    add assets and intro page

--- index.php
+++ index.php
@@ -1,10 +1,69 @@
 <html>
-    <head>
-        <title>
-            It works...
-        </title>
-    </head>
-    <body>
-        It works...
-    </body>
+
+<head>
+    <title>
+        Mini Weebly
+    </title>
+    <link rel="stylesheet" type="text/css" href="stylesheets/reset.css">
+    <link rel="stylesheet" type="text/css" href="stylesheets/styles.css">
+</head>
+
+<body>
+    <div id="header">
+        <img src="images/Logomark.png" id="logo">
+    </div>
+    <div class="sidebarNav">
+
+        <div class="sidebarTab">
+            <div class="sidebarTabText">
+                TEMPLATES
+            </div>
+            <div class="sidebarElementContainer">
+                <div class="pageButtonContainer">
+                    <div>
+                        PAGE
+                    </div>
+                </div>
+                <div class="addNewPage">
+                    ADD NEW PAGE
+                </div>
+            </div>
+        </div>
+
+        <div class="sidebarTab">
+            <div class="sidebarTabText">
+                ELEMENTS
+            </div>
+            <div class="sidebarElementContainer">
+                <div class="elementsTile" id="title">
+                </div>
+                <div class="elementsTile" id="text">
+                </div>
+                <br>
+                <div class="elementsTile" id="image">
+                </div>
+                <div class="elementsTile" id="nav">
+                </div>
+            </div>
+        </div>
+
+        <div class="sidebarTab">
+            <div class="sidebarTabText">
+                SETTINGS
+            </div>
+            <div class="sidebarElementContainer">
+                <div class="settingsTabContainer">
+                    <div id="settingsTabText">
+                        SITE GRID
+                    </div>
+                </div>
+                <div id="settingsTabToggleContainer">
+                    <img src="images/Off.png" id="off">
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
 </html>
