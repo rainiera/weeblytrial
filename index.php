50c8d3ace9adcfd614cfb9402bf28289e3a1e73c
Rainier Ababao <rainier.ababao@gmail.com>
2015-03-31 13:46:51 -0700

    add connection to the databae

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
-<?php session_start(); ?>
+<?php
+    session_start();
+
+
+    // Make a database connection
+    $dbhost = "localhost";
+    $dbuser = "weeblytrial_cms";
+    $dbpass = "pingpong";
+    $dbname = "weeblytrial";
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); // make a connection handle
+
+    // See if connection succeeded
+    if(mysqli_connect_errno()) {
+        die("Database connection failed: " .
+            mysqli_connect_error() . 
+            " (" .mysqli_connect_errno() . ")"
+        );
+    }
+
+    // Perform database query
+    $query = "SELECT * ";
+    $query .= "FROM pages ";
+    $query .= "WHERE visible = 1 ";
+    $query .= "ORDER BY position ASC";
+    $result = mysqli_query($connection, $query); // creates a resource object
+    // test if there's a query error
+    if (!$result) {
+        die("Database query failed.");
+    }
+
+    $numPages = 1;
+    $pageNames = {}
+
+    while($row = mysqli_fetch_assoc($result)) {
+    // output data from each row
+    var_dump($row);
+    echo $row["pageName"] . "<br>";
+}
+
+?>
 
 <html>
 
@@ -9,10 +48,11 @@
     <link rel="stylesheet" type="text/css" href="stylesheets/proximanovas/stylesheet.css">
     <link rel="stylesheet" type="text/css" href="stylesheets/reset.css">
     <link rel="stylesheet" type="text/css" href="stylesheets/styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="javascripts/newpage.js"></script>
 </head>
 
 <body>
-    <?php //$numPages=1 ; //$_SESSION[ "$numPages"]=1 ; //echo numPages; //echo mt_rand() ?>
 
     <div id="header">
         <img src="stylesheets/sprites/Weebly-Logo.png" id="logo">
@@ -42,7 +82,7 @@
                 </div>
                 <div class="pageButton" id="addNewPage">
                     <div id="addNewPageText">
-                        ADD NEW PAGE
+                        <input id="addNewPage" id="addNewPageText" placeholder="ADD NEW PAGE">
                     </div>
                 </div>
             </div>
@@ -65,14 +105,14 @@
                 <div class="elementsTileText">TITLE</div>
             </div>
 
-<!--             <div class="vertDivider"></div>
+            <!--             <div class="vertDivider"></div>
  -->
             <div class="elementsTile">
                 <img src="stylesheets/sprites/Element-IconsText.png" class="tileImg">
                 <div class="elementsTileText">TEXT</div>
             </div>
 
-        <div class="horizDivider"></div>
+            <div class="horizDivider"></div>
 
             <div class="elementsTile">
                 <img src="stylesheets/sprites/Element-IconsImage.png" class="tileImg">
@@ -98,26 +138,59 @@
 
         <div class="horizDivider"></div>
 
-            <div class="sidebarElementContainer">
-                <div id="settingsTabText">
-                    SITE GRID
-                </div>
-                <div id="settingsTabToggleContainer">
-                    <img src="images/Off.png" id="off">
-                </div>
+        <div class="sidebarElementContainer">
+            <div id="settingsTabText">
+                SITE GRID
+            </div>
+            <div id="settingsTabToggleContainer">
+                <img src="images/Off.png" id="off">
             </div>
         </div>
-
     </div>
+
 
     <!-- START MAIN CONTENT -->
 
-    <div id="mainContentContainer">
-        <div id="mainContentPageButtonContainer">
+    <div id="mainContentContainerContainer">
+        <div id="mainContentContainer">
             <div id="mainContentPageButtonContainer">
+
                 <div class="activePageButton">
                     <div class="activePageButtonText">
                         PAGE
+                    </div>
+                </div>
+
+                <!-- whenever addNewPageButton with text entered is pressed,
+                    add a new inactive page -->
+
+                <?php
+
+                function addInactivePage($newPageName) {
+                    $inactivePageButton = "<div class=\"inactivePageButton\">
+                    <div class=\"inactivePageButtonText\">";
+                    $inactivePageButton .= $newPageName;
+                    $inactivePageButton .= "</div></div>";
+
+                    echo $inactivePageButton;
+                }
+
+
+                if($numPages > 1) {
+                    for ($i = 0; $i < $numPages - 1; $i++) {
+                        addInactivePage("YOLO");
+                    }
+                }
+
+                ?>
+
+            </div>
+
+            <div id="mainContentImageContainerContainer">
+                <div id="mainContentImageContainer">
+                    <img src="images/Icon.png" class="mainContentImage">
+                    <div class="mainContentImageText">
+                        ADD IMAGE <div id="plus">+</div>
                     </div>
                 </div>
             </div>
